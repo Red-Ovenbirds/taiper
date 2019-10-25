cb8faa8c7138a657e10b4ff2e7bea7c2fa7e4620
Alyson Noronha <alyson_nbs2.0@hotmail.com>
2019-10-25 08:57:09 -0300

    VideoClayblock alpha

--- lib/src/clayblocks/video.dart
+++ lib/src/clayblocks/video.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 import './clayblock.dart';
+import 'video/local_web_player.dart';
+import 'video/youtube_player.dart';
 
 class VideoClayblock extends Clayblock {
+  final identifier = "video";
+
+  final VideoClayblockType type;
+  final String src;
+
+  VideoClayblock({
+    @required this.src,
+    this.type = VideoClayblockType.youtube,
+  });
   
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    switch(type) {
+      case VideoClayblockType.local:
+        return ChewiePlayer(videoPlayerController: VideoPlayerController.asset(src));
+      case VideoClayblockType.web:
+        return ChewiePlayer(videoPlayerController: VideoPlayerController.network(src));
+      case VideoClayblockType.youtube:
+        //TODO: implement youtube player
+      default:
+        return null;
+    }
   }
 
+}
+
+enum VideoClayblockType {
+  local, web, youtube
 }
---
+++ lib/src/clayblocks/video/local_web_player.dart
@@ -0,0 +1,58 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class ChewiePlayer extends StatefulWidget {
+  final VideoPlayerController videoPlayerController;
+  final bool looping;
+
+  ChewiePlayer({
+    @required this.videoPlayerController,
+    this.looping,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  State createState() => ChewiePlayerState();
+}
+
+class ChewiePlayerState extends State<ChewiePlayer> {
+  ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _chewieController = ChewieController(
+      videoPlayerController: widget.videoPlayerController,
+      aspectRatio: 16 / 9,
+      autoInitialize: true,
+      looping: widget.looping,
+      errorBuilder: (context, errorMessage) {
+        return Center(
+          child: Text(
+            errorMessage,
+            style: TextStyle(color: Colors.white),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Chewie(
+        controller: _chewieController,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    widget.videoPlayerController.dispose();
+    _chewieController.dispose();
+  }
+}
