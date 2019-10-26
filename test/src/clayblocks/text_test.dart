a1872d5e832b8ed2e985c0364b9ab0bfb53a4c49
Régis Melgaço de Andrade <regis@Regiss-MacBook-Pro.local>
2019-10-26 09:19:14 -0300

    a lot of tests

--- test/src/clayblocks/text_test.dart
+++ test/src/clayblocks/text_test.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:taiper/src/clayblocks/text.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:taiper/taiper.dart';
 import '../../util.dart';
 
 void main() {
-  testWidgets("Test of text of title type", (WidgetTester tester) async {
+  testWidgets("TextClayblock type title", (WidgetTester tester) async {
     final title = "My title";
 
     await pumpWithMaterial(
@@ -12,12 +14,100 @@ void main() {
     expect(find.text(title), findsOneWidget);
   });
 
-  testWidgets("Test of text of message type", (WidgetTester tester) async {
-    final message = "My message";
+  testWidgets("TextClayblock type body", (WidgetTester tester) async {
+    final messenge = "My messenge";
 
     await pumpWithMaterial(
-        tester, TextClayblock(type: TextClayblockType.body, content: message));
+        tester, TextClayblock(type: TextClayblockType.body, content: messenge));
 
-    expect(find.text(message), findsOneWidget);
+    expect(find.text(messenge), findsOneWidget);
+  });
+
+  testWidgets("TextClayblock type ULItem", (WidgetTester tester) async {
+    final messenge = "My messenge";
+
+    await pumpWithMaterial(
+        tester, TextClayblock(type: TextClayblockType.ULItem, content: messenge));
+
+    expect(find.text(messenge), findsOneWidget);
+  });
+
+  testWidgets("Text builder for body", (WidgetTester tester) async {
+    final messenge = "My messenge";
+
+    await pumpWithMaterial(tester,
+        TextClayblockFactory().build(messenge, "text/body"));
+
+    expect(find.text(messenge), findsOneWidget);
+  });
+
+  testWidgets("Text builder for title", (WidgetTester tester) async {
+    final messenge = "My messenge";
+
+    await pumpWithMaterial(tester,
+        TextClayblockFactory().build(messenge, "text/title"));
+
+    expect(find.text(messenge), findsOneWidget);
+  });
+
+  testWidgets("Text builder for ulitem", (WidgetTester tester) async {
+    final messenge = "My messenge";
+
+    await pumpWithMaterial(tester,
+        TextClayblockFactory().build(messenge, "text/ulitem"));
+
+    expect(find.text(messenge), findsOneWidget);
+  });
+
+  test("ClayblockData", () {
+    final type = "typeee";
+    final value = "valueee";
+
+    final cbData = ClayblockData(type: type, value: value);
+
+    expect(cbData.type, type);
+    expect(cbData.value, value);
+  });
+
+  testWidgets("TaipaBuilder", (WidgetTester tester) async {
+    final type1 = "text/body";
+    final value1 = "valueee";
+    final type2 = "text/title";
+    final value2 = "valueeeee";
+
+    final cbDatas = [
+      ClayblockData(type: type1, value: value1),
+      ClayblockData(type: type1, value: value1),
+      ClayblockData(type: type1, value: value2),
+      ClayblockData(type: type2, value: value2),
+    ];
+
+    await pumpWithMaterial(tester, Row(
+      children: TaipaBuilder().construct(cbDatas),
+    ));
+
+    expect(find.text(value1), findsNWidgets(2));
+    expect(find.text(value2), findsNWidgets(2));
+  });
+
+  testWidgets("TaiperContainer", (WidgetTester tester) async {
+    final type1 = "text/body";
+    final value1 = "valueee";
+    final type2 = "text/title";
+    final value2 = "valueeeee";
+
+    final cbDatas = [
+      ClayblockData(type: type1, value: value1),
+      ClayblockData(type: type1, value: value1),
+      ClayblockData(type: type1, value: value1),
+      ClayblockData(type: type2, value: value2),
+    ];
+
+    await pumpWithMaterial(tester, TaiperContainer(
+      TaipaBuilder().construct(cbDatas),
+    ));
+
+    expect(find.text(value1), findsNWidgets(3));
+    expect(find.text(value2), findsOneWidget);
   });
 }
