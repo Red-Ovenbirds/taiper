593e3ea60207e257a06e0bd4111b2302f5ae60c1
Alyson Noronha <alyson_nbs2.0@hotmail.com>
2019-11-04 11:58:34 -0300

    incomplete test of ImageClayblock

---
+++ test/src/clayblocks/image_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:image_test_utils/image_test_utils.dart';
+import '../../util.dart';
+import 'package:taiper/src/clayblocks/image.dart';
+
+void main() {
+  testWidgets("Web Image build test", (WidgetTester tester) async {
+    provideMockedNetworkImages(() async {
+      final image =  ImageClayblock(type: ImageClayblockType.web, src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Flickr_-_Dario_Sanches_-_JO%C3%83O-DE-BARRO_%28Furnarius_rufus%29_%285%29.jpg/250px-Flickr_-_Dario_Sanches_-_JO%C3%83O-DE-BARRO_%28Furnarius_rufus%29_%285%29.jpg");
+
+      await pumpWithMaterial(tester, image);
+
+      expect(find.byWidget(image), findsOneWidget);
+    });
+  });
+}
