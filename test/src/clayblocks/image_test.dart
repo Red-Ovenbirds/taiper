36276e6386621304abbafd4e7dfc321cd3f4ddc5
RegisMelgaco <regis.melgaco@gmail.com>
2019-11-25 10:31:57 -0300

    Web ImageFactory test

--- test/src/clayblocks/image_test.dart
+++ test/src/clayblocks/image_test.dart
@@ -4,9 +4,19 @@ import '../../util.dart';
 import 'package:taiper/src/clayblocks/image.dart';
 
 void main() {
-  testWidgets("Web Image build test", (WidgetTester tester) async {
+  testWidgets("Web ImageClayblock", (WidgetTester tester) async {
     provideMockedNetworkImages(() async {
       final image =  ImageClayblock(type: ImageClayblockType.web, src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Flickr_-_Dario_Sanches_-_JO%C3%83O-DE-BARRO_%28Furnarius_rufus%29_%285%29.jpg/250px-Flickr_-_Dario_Sanches_-_JO%C3%83O-DE-BARRO_%28Furnarius_rufus%29_%285%29.jpg");
+
+      await pumpWithMaterial(tester, image);
+
+      expect(find.byWidget(image), findsOneWidget);
+    });
+  });
+
+  testWidgets("Web ImageFactory", (WidgetTester tester) async {
+    provideMockedNetworkImages(() async {
+      final image =  ImageClayblockFactory().build("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Flickr_-_Dario_Sanches_-_JO%C3%83O-DE-BARRO_%28Furnarius_rufus%29_%285%29.jpg/250px-Flickr_-_Dario_Sanches_-_JO%C3%83O-DE-BARRO_%28Furnarius_rufus%29_%285%29.jpg", "web");
 
       await pumpWithMaterial(tester, image);
 
