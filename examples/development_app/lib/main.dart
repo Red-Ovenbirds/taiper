fd30d53337cef09804b78dd2073a6988005a7e9d
Jagni Dasa <jagnidasa@gmail.com>
2019-12-13 11:11:08 -0300

    Conflict fix

--- examples/development_app/lib/main.dart
+++ examples/development_app/lib/main.dart
@@ -24,49 +24,9 @@ class MyHomePage extends StatelessWidget {
         title: Text("Taiper Example"),
       ),
       body: Center(
-<<<<<<< HEAD
         child: TaiperContainer(
           TaipaBuilder().construct(exampleData),
         ),
-=======
-        child: TaiperContainer([
-          TextClayblock(
-            type: TextClayblockType.title,
-            content: "Hello World. The first title.",
-          ),
-          TextClayblock(
-            type: TextClayblockType.body,
-            content:
-              "Example of text in body.",
-          ),
-          TextClayblock(
-            type: TextClayblockType.body,
-            content:
-              "A new example of text in body. More, more, more, more, more, more, more, more, more, more, more, more, more and more words to fill some lines",
-          ),
-          TextClayblock(
-            type: TextClayblockType.title,
-            content: "The second title",
-          ),
-          TextClayblock(
-            type: TextClayblockType.body,
-            content:
-              "A third text of body type. It's ok?",
-          ),
-          ImageClayblock(
-            type: ImageClayblockType.local,
-            src: "assets/Red-Ovenbird.jpg",
-          ),
-          ImageClayblock(
-            type: ImageClayblockType.web,
-            src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Flickr_-_Dario_Sanches_-_JO%C3%83O-DE-BARRO_%28Furnarius_rufus%29_%285%29.jpg/250px-Flickr_-_Dario_Sanches_-_JO%C3%83O-DE-BARRO_%28Furnarius_rufus%29_%285%29.jpg",
-          ),
-          VideoClayblock(
-            src: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-            type: VideoClayblockType.web,
-          )
-        ]),
->>>>>>> Video example and usage of new cached_image pub
       ),
     );
 
