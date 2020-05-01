0163becdd417ac0fe0e52070b7f29e6f7bb06b88
Jagni Dasa <jagnidasa@gmail.com>
2020-04-30 22:26:47 -0300

    Primary attribute

--- lib/src/container.dart
+++ lib/src/container.dart
@@ -5,15 +5,18 @@ class TaiperContainer extends StatelessWidget {
   final List<Clayblock> clayblocks;
   final EdgeInsets padding;
   final bool shrinkWrap;
+  final bool primary;
   final Axis scrollDirection;
   TaiperContainer(this.clayblocks,
       {this.padding = const EdgeInsets.all(8),
       this.shrinkWrap = false,
+      this.primary = true,
       this.scrollDirection = Axis.vertical});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      primary: primary,
       padding: padding,
       shrinkWrap: shrinkWrap,
       scrollDirection: scrollDirection,
